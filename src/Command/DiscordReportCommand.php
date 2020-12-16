8466c80a7b93a8dce31f4c0590818d2480e21763
NiclasTi <niclas.titius@nicti.de>
2020-12-16 22:31:09 +0100

    Fix commands

--- src/Command/DiscordReportCommand.php
+++ src/Command/DiscordReportCommand.php
@@ -1,18 +1,11 @@
-<?php
+<?php /** @noinspection PhpMissingFieldTypeInspection */
 
 namespace App\Command;
 
-use App\Entity\DiscordRole;
 use App\Error\DiscordHandler;
-use App\Repository\DiscordRoleRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use GuzzleHttp\Client;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 class DiscordReportCommand extends Command
 {
